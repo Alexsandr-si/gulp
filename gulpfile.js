53c930e519cd4d461dfd92941d97d2f13a060d2d
master <master@MacBook-Pro.local>
2018-01-23 14:00:20 +0200

    first

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browsersync = require('browser-sync'),
+    concat      = require('gulp-concat'),
+    ugli        = require('gulp-uglifyjs'),
+    cssnano     = require('gulp-cssnano'),
+    rename      = require('gulp-rename'),
+    imagemin    = require('gulp-imagemin');
+gulp.task('image', function(){
+    return gulp.src('app/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+});
+gulp.task('cssnano', ['sasswork'], function(){
+    return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+});
+gulp.task('scripts', function(){
+    return gulp.src(['app/libs/jquery/dist/jquery.min.js','app/libs/magnific-popup/dist/jquery.magnific-popup.min.js','app/libs/parallax/deploy/jjquery.parallax.min.js',])
+    .pipe(concat('libs.min.js'))
+    .pipe(ugli())
+    .pipe(gulp.dest('app/js'))
+});
+gulp.task('sasswork', function(){
+    return gulp.src('app/sass/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browsersync.reload({stream: true}))
+});
+gulp.task('br-sync',function(){
+   browsersync({
+       server: {
+           baseDir: 'app'
+       },
+       notify: false
+   });
+});
+gulp.task('watch', ['br-sync', 'cssnano'], function(){
+    gulp.watch('app/sass/*.sass',['sasswork']);
+    gulp.watch('app/*.html', browsersync.reload);
+    gulp.watch('app/js/**/*.js', browsersync.reload);
+});
