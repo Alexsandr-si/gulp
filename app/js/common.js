7be75720eba5a7ad76d95ff1e4badcc9fb4937a7
master <master@MacBook-Pro.local>
2018-01-24 23:12:46 +0200

    23:10

--- app/js/common.js
+++ app/js/common.js
@@ -9,4 +9,24 @@ $(document).ready(function() {
     }).bind("close:finish",function(){
         $('.hamburger').removeClass('is-active');
     }); 
+    $('.carousel-services').owlCarousel({
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+        rresponsiveClass:true,
+        responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+             720:{
+            items:2,
+            
+        },
+        1100:{
+            items:3,
+        }
+    }
+    });
 });
