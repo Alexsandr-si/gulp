77590a800edd8ddbc5e0ebb78dd377f705e4f1b6
Aleksandr Kolesnikov <ises2000@mail.ru>
2018-01-24 14:26:16 +0200

    14:26

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,12 @@
- $(document).ready(function () {
-     $("#my-menu").mmenu({
-         // options
-     }, {
-         // configuration
-         offCanvas: {
-             pageNodetype: "section"
-         }
-     });
- });
+$(document).ready(function() {
+     $('#my-menu').mmenu({
+         "extensions": [ "position-back", "position-right", "effect-menu-slide"],
+          navbar: { title: '<img src="img/logo.png" alt="РосАтомАудит">'}
+    });
+    var API = $("#my-menu").data( "mmenu" );
+    API.bind("open:finish",function(){
+        $('.hamburger').addClass('is-active');
+    }).bind("close:finish",function(){
+        $('.hamburger').removeClass('is-active');
+    }); 
+});
