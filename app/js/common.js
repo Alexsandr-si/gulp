494b24d9b828882ccb06bc4a4e04c0a9425cc278
Aleksandr Kolesnikov <ises2000@mail.ru>
2018-01-26 15:14:52 +0200

    26.01

--- app/js/common.js
+++ app/js/common.js
@@ -9,23 +9,24 @@ $(document).ready(function() {
     }).bind("close:finish",function(){
         $('.hamburger').removeClass('is-active');
     }); 
-    $('.carousel-services').owlCarousel({
+    $('.carousel-services').owlCarousel(
+        {
         loop: true,
         nav: true,
         smartSpeed: 700,
         navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
         rresponsiveClass:true,
         responsive:{
-        0:{
-            items:1,
-            nav:true
+        0: {
+            items: 1,
+            nav: true
         },
-             720:{
-            items:2,
+             720: {
+            items: 2,
             
         },
-        1100:{
-            items:3,
+        1100: {
+            items: 3,
         }
     }
     });
@@ -34,20 +35,33 @@ $(document).ready(function() {
         smartSpeed: 700,
         nav: true,
         navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
-        rresponsiveClass:true,
-        responsive:{
-        0:{
-            items:1,
-            nav:true
+        rresponsiveClass: true,
+        responsive: {
+        0: {
+            items: 1,
+            nav: true
         },
-             720:{
-            items:2,
+             720: {
+            items: 2,
             
         },
-        1100:{
-            items:3,
+        1100: {
+            items: 3,
         }}
     });
+    
+    $(window).scroll(function() {
+        if($(this).scrollTop() > $(this).height()){
+            $('.top_up').css('bottom', '20px')
+        }
+        else {
+            $('.top_up').css('bottom', '-200px')
+        }
+    });
+    $('.top_up').click(function() {
+        $('html').stop().animate({scrollTop: 0}, 'slow', 'swing');
+    });
+    
     $(".main_form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
